8ad059a684992b4eff3e95d48e38e45d503ef2d6
Andreas Bazigos <sc222ab@leeds.ac.uk>
2025-10-06 19:44:17 +0100

    branched to dev

--- multi-threading/no-threads.py
+++ multi-threading/no-threads.py
@@ -2,22 +2,36 @@ import os
 import re
 from time import time
 import sys
+
 lifeline = re.compile(r"(\d) received")
-report = ("No response","Partial Response","Alive")
+report = ("No response", "Partial Response", "Alive")
 start=time()
+
+post_report=[0, 0, 0]
 for host in range(1,49):
 #   ip = "129.11.146."+str(host)
-   ip = "90.194.19."+str(host)
-   pingaling = os.popen("ping -q -c2 "+ip,"r")
-   print ("Testing ",ip,
-   sys.stdout.flush())
+   ip = "90.194.19." + str(host)
+   pingaling = os.popen("ping -q -c2 " + ip, "r")
+   print ("Testing ", ip, sys.stdout.flush(), end=" -> ")
+   
    while 1:
       line = pingaling.readline()
       if not line: break
-      igot = re.findall(lifeline,line)
+      igot = re.findall(lifeline, line)
+      
       if igot:
-           print (report[int(igot[0])])
+         res = report[int(igot[0])]
+         print (res)
+         
+         if res == report[0]:
+            post_report[0]+=1
+         elif res == report[1]:
+            post_report[1]+=1
+         elif res == report[2]:
+            post_report[2]+=1
+         
 stop=time()
-elapsed = stop -start
-print ("Time elapsed", elapsed)
-print ("done.")
+elapsed = stop - start
+
+print (f"Time elapsed {elapsed} - done.")
+print(f"{report[0]}({post_report[0]} {report[1]}({post_report[1]} {report[2]}({post_report[2]})")
--- multi-threading/with-threads.py
+++ multi-threading/with-threads.py
@@ -3,34 +3,41 @@ import re
 from time import time
 import sys
 from threading import Thread
+
 class testit(Thread):
-   def __init__ (self,ip):
+   def __init__ (self, ip):
       Thread.__init__(self)
       self.ip = ip
       self.status = -1
+      
    def run(self):
-      pingaling = os.popen("ping -q -c2 "+self.ip,"r")
+      pingaling = os.popen("ping -q -c2 " + self.ip, "r")
       while 1:
         line = pingaling.readline()
         if not line: break
-        igot = re.findall(testit.lifeline,line)
+        igot = re.findall(testit.lifeline, line)
         if igot:
            self.status = int(igot[0])
+           
+           
 testit.lifeline = re.compile(r"(\d) received")
-report = ("No response","Partial Response","Alive")
+report = ("No response", "Partial Response", "Alive")
 #print time.ctime()
 start=time()
 pinglist = []
+
 for host in range(1,49):
 #   ip = "129.11.144."+str(host)
-   ip = "90.194.19."+str(host)
+   ip = "90.194.19." + str(host)
    current = testit(ip)
    pinglist.append(current)
    current.start()
+   
 for pingle in pinglist:
    pingle.join()
-   print ("Status from ",pingle.ip,"is",report[pingle.status])
+   print ("Status from ", pingle.ip, "is", report[pingle.status])
+   
 stop=time()
 elapsed = stop -start
-print ("Time elapsed", elapsed)
-print ("done.")
+
+print (f"Time elapsed {elapsed} - done.")
